167f3bad43f9f77b35438b7fd5ef8951478ce022
ICRA <154600112+icratech@users.noreply.github.com>
2023-12-28 21:11:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 let speech = new SpeechSynthesisUtterance();
-let voices = [];
-let voiceSelect = document.getElementById("voiceSelect");
-let listenButton = document.getElementById("listenButton");
-let saveButton = document.getElementById("saveButton");
+let voices = window.speechSynthesis.getVoices();
 
+// Ses seçimi için bir <select> elementi
+let voiceSelect = document.getElementById("voiceSelect");
+
+// Textarea elementi
+let textArea = document.getElementById("text");
+
+// Ses değiştirildiğinde event listener
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
-  speech.voice = voices[0];
+
+  voiceSelect.innerHTML = ""; // Mevcut seçenekleri temizle
 
   voices.forEach((voice, i) => {
-    voiceSelect.options[i] = new Option(voice.name, i);
+    let option = document.createElement("option");
+    option.value = i;
+    option.textContent = voice.name;
+    voiceSelect.appendChild(option);
   });
+
+  // Default sesi ayarla
+  speech.voice = voices[0];
 };
 
+// Ses seçimi değiştiğinde event listener
 voiceSelect.addEventListener("change", () => {
   speech.voice = voices[voiceSelect.value];
 });
 
-listenButton.addEventListener("click", () => {
-  speech.text = document.getElementById("textInput").value;
+// Listen butonu için event listener
+document.getElementById("listenButton").addEventListener("click", () => {
+  speech.text = textArea.value;
   window.speechSynthesis.speak(speech);
 });
 
-saveButton.addEventListener("click", () => {
-  const textToSave = document.getElementById("textInput").value;
+// Save butonu için event listener
+document.getElementById("saveButton").addEventListener("click", () => {
+  const textToSave = textArea.value;
   const blob = new Blob([textToSave], { type: "text/plain" });
   const a = document.createElement("a");
   a.href = URL.createObjectURL(blob);
