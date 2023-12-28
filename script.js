728b2749cc127b9fcc0e38fe74eb81b7094fb79d
ICRA <154600112+icratech@users.noreply.github.com>
2023-12-28 21:08:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 let speech = new SpeechSynthesisUtterance();
 let voices = [];
-let voiceSelect = document.querySelector("select");
+let voiceSelect = document.getElementById("voiceSelect");
+let listenButton = document.getElementById("listenButton");
+let saveButton = document.getElementById("saveButton");
 
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
   speech.voice = voices[0];
-  voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+
+  voices.forEach((voice, i) => {
+    voiceSelect.options[i] = new Option(voice.name, i);
+  });
 };
 
 voiceSelect.addEventListener("change", () => {
   speech.voice = voices[voiceSelect.value];
 });
 
-document.querySelector("button.listen").addEventListener("click", () => {
-  speech.text = document.querySelector("textarea").value;
-  window.speechSynthesis.cancel(); // Önceki konuşmayı iptal et
+listenButton.addEventListener("click", () => {
+  speech.text = document.getElementById("textInput").value;
   window.speechSynthesis.speak(speech);
 });
 
-document.querySelector("button.save").addEventListener("click", () => {
-  let textToSave = document.querySelector("textarea").value;
-  let blob = new Blob([textToSave], { type: "text/plain" });
-  let anchor = document.createElement("a");
-  anchor.href = URL.createObjectURL(blob);
-  anchor.download = "text_to_speech.txt";
-  anchor.click();
+saveButton.addEventListener("click", () => {
+  const textToSave = document.getElementById("textInput").value;
+  const blob = new Blob([textToSave], { type: "text/plain" });
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = "textToSpeech.txt";
+  a.click();
 });
